0322fb26f567e96797d34e9906515f7f235e7916
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-04-01 12:58:02 -0400

    Add files via upload

---
+++ invertTree.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode left_side = invertTree(root.left);
+        TreeNode right_side = invertTree(root.right);
+
+        root.left = right_side;
+        root.right = left_side;
+
+        return root;
+
+    }
+}
---
+++ maxDepth.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        if(root == null) return 0;
+
+        int left_height = maxDepth(root.left);
+        int right_height = maxDepth(root.right);
+
+        return Math.max(left_height, right_height) + 1;
+
+    }
+
+    
+
+
+}
